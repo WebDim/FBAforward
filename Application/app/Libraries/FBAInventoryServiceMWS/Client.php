a208bcbc04eb0ac35e0a8d2f7741d2f27f4573f0
Megha Jogi <megha.n.jogi@gmail.com>
2017-01-07 17:38:00 +0530

    amazon invetory bug fix
    amazon invetory bug fix

--- Application/app/Libraries/FBAInventoryServiceMWS/Client.php
+++ Application/app/Libraries/FBAInventoryServiceMWS/Client.php
@@ -277,6 +277,7 @@ class FBAInventoryServiceMWS_Client implements FBAInventoryServiceMWS_Interface
         //iconv_set_encoding('input_encoding', 'UTF-8');
         //iconv_set_encoding('internal_encoding', 'UTF-8');
 
+
         $this->_awsAccessKeyId = $awsAccessKeyId;
         $this->_awsSecretAccessKey = $awsSecretAccessKey;
         $this->_serviceVersion = $applicationVersion;
