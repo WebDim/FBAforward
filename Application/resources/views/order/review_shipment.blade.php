185c01c08771c455ffdccfae6a6258f78a001b9f
Megha Jogi <megha.n.jogi@gmail.com>
2017-01-21 18:12:34 +0530

    outbound shipping and review shipment functionality

---
+++ Application/resources/views/order/review_shipment.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.frontend.app')
+
+@section('title', 'Review Shipment Information')
+
+@section('content')
+    @include('layouts.frontend.tabs', ['data' => 'review_shipment'])
+    <div class="row">
+        <div class="col-md-12">&nbsp;</div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            {!! Form::open(['url' =>  'order/payment', 'method' => 'put', 'files' => true, 'class' => 'form-horizontal', 'id'=>'validate']) !!}
+            <div class="row">
+                <div class="col-md-6">
+                    <h4>Shipments</h4>
+                    <div class="table-responsive no-padding">
+                        <table class="table" id="list">
+                            <thead>
+                            <tr>
+                                <th class="col-md-3"><span>Shipment</span></th>
+                                <th class="col-md-5"><span>Total Units</span></th>
+                                <th class="col-md-2"><span>Methods</span></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($shipment as $key=>$shipments)
+                                <tr>
+                                <td>Shipment #{{$key+1}}</td>
+                                    <td>{{ $shipments->total }}</td>
+                                    <td>{{ $shipments->shipping_name }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <h4>Outbound</h4>
+                    <div class="table-responsive no-padding">
+                        <table class="table" id="list">
+                            <thead>
+                            <tr>
+                                <th class="col-md-3"><span>Amazon Destination</span></th>
+                                <th class="col-md-5"><span>Units</span></th>
+                                <th class="col-md-2"><span>Outbound Method</span></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($outbound_detail as $outbound_details)
+                                <tr>
+                                    <td class="col-md-3"><span>{{ $outbound_details->destination_name }}</span></td>
+                                    <td class="col-md-5"><span>
+                                          {{--*/$qty=array()/*--}}
+                                            {{--*/$qty=explode(',',$outbound_details->qty)/*--}}
+                                            {{--*/$total_qty=0/*--}}
+                                            @foreach($qty as $qtys)
+                                                {{--*/$total_qty=$total_qty+$qtys/*--}}
+                                                @endforeach
+                                            {{ $total_qty }}
+                                        </span></td>
+                                    <td class="col-md-2"><span>{{ $outbound_details->outbound_name }}</span></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h4>Products</h4>
+                    <div class="table-responsive no-padding">
+                        <table class="table" id="list">
+                            <thead>
+                            <tr>
+                                <th class="col-md-5"><span>Product</span></th>
+                                <th class="col-md-2"><span>Qty</span></th>
+                                <th class="col-md-2"><span>Services</span></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($product_detail as $product_details)
+
+                                <tr>
+                                    <td>{{ $product_details->product_name }}</td>
+                                    <td>{{ $product_details->total }}</td>
+                                    <td>
+                                        {{--*/$prep_ids=array()/*--}}
+                                        {{--*/$prep_ids=explode(',',$product_details->prep_service_ids)/*--}}
+                                        @foreach($prep_ids as $ids)
+                                        @foreach($prep_service as $prep_services)
+                                            @if($prep_services->prep_service_id==$ids)
+                                                {{ $prep_services->service_name  }}<br>
+                                            @endif
+                                        @endforeach
+                                        @endforeach
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="form-group">
+                <div class="col-md-9 col-md-offset-9">
+                    <a href="{{ URL::route('outbondshipping') }}" class="btn btn-primary">Previous</a>
+                    {!! Form::submit('  Next  ', ['class'=>'btn btn-primary']) !!}
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
