d3f86bbc852963104542802fffeddc56d3358710
Megha Jogi <megha.n.jogi@gmail.com>
2017-02-23 13:26:40 +0530

    bug fix
    bug fix

--- Application/app/Invoice_detail.php
+++ Application/app/Invoice_detail.php
@@ -9,7 +9,7 @@ class Invoice_detail extends Model
     //
     protected $primaryKey = 'id';
 
-    protected $table ='Invoice_details';
+    protected $table ='invoice_details';
     protected $fillable=['invoice_id','created_time','updtaed_time','docnumber','txndate','customer_ref','customer_ref_name','line1','line2','city','country','postalcode','lat','due_date','total_amt','currancy_ref','currency_ref_name','total_taxe'];
 
     public function invoice_product_detail()
